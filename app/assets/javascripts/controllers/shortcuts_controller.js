f688aeefec742ee6955961fd51c642216e2ea485
Rodrigo Ra <rodrigorcomp@gmail.com>
2014-05-19 23:37:23 -0300

    JS validation

--- app/assets/javascripts/controllers/shortcuts_controller.js
+++ app/assets/javascripts/controllers/shortcuts_controller.js
@@ -1,6 +1,24 @@
 SublimeShortcuts.ShortcutsController = Ember.ArrayController.extend({
+  keyErrorBuff: false,
+  titleErrorBuff: false,
   actions: {
     createShortcut: function() {
+
+      if(!this.get('newShortcutTitle')){
+        this.set('titleError', true)
+      }else{
+        this.set('titleError', false);
+      }
+
+      if(!this.get('newShortcutKey')){
+        this.set('keyError', true);
+      }else{
+        this.set('keyError', false);
+      }
+
+      if(this.get('keyError') || this.get('titleError')){
+        return;
+      }
 
       var shortcut = this.store.createRecord('shortcut', {
         title: this.get('newShortcutTitle'),
@@ -12,5 +30,22 @@ SublimeShortcuts.ShortcutsController = Ember.ArrayController.extend({
 
       shortcut.save();
     }
-  }
+  },
+  hasError: Ember.computed('keyError', 'titleError', function() {
+    return this.get('keyError') || this.get('titleError');
+  }),
+
+  keyError: function(arg, value){
+    if(value != undefined){
+      this.set('keyErrorBuff', value);
+    }
+    return this.get('keyErrorBuff');
+  }.property(),
+
+  titleError: function(arg, value){
+    if(value != undefined){
+      this.set('titleErrorBuff', value);
+    }
+    return this.get('titleErrorBuff');
+  }.property()
 });
